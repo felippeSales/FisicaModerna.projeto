5b1a770ea9dcdcfac42d095a07f2ae681937fc1d
Felipe Sales <sales.felipeb@gmail.com>
2014-03-30 02:49:13 -0300

    Terminando de por imagens

--- play.py
+++ play.py
@@ -43,6 +43,7 @@ class Personagem(object):
 			if (self.x + self.move_speed) <= 600:
 				self.x += self.move_speed
 
+		print "Y:" , self.y , " X: " , self.x
 		
 	# Poe uma imagem no objeto: 
 	def set_surface(self, imagem):
@@ -116,13 +117,85 @@ class Sensor(object):
 	def draw(self, surface):
 		surface.blit(self.surface, (self.x, self.y))
 			
+class Raio(object):
+	def __init__(self):
+		self.surface = pygame.image.load("Imagens/portaAutomatica/raioVertical.png").convert()
+		self.surface.set_colorkey((0,0,0))
+		
+		#posicao do puto
+		self.x = 570
+		self.y = 150
+
+	def set_surface(self, imagem):
+		self.surface = pygame.image.load(imagem).convert()
+		self.surface.set_colorkey((0,0,0))
+		
+	def draw(self, surface):
+		self.y += 1
+		surface.blit(self.surface, (self.x, self.y))
+		
+		if self.y >= 440:
+			 self.y = 150
+		
+class Porta_Esquerda(object):
+	def __init__(self):
+		self.surface = pygame.image.load("Imagens/portaAutomatica/porta.png").convert()
+		self.surface.set_colorkey((0,0,0))
+		
+		#posicao do puto
+		self.x = 515
+		self.y = 204
+
+	def set_surface(self, imagem):
+		self.surface = pygame.image.load(imagem).convert()
+		self.surface.set_colorkey((0,0,0))
+	
+	def abre_porta(self):
+		self.x -= 1
+		if self.x == 408:
+			self.x = 515
+	
+	def draw(self, surface):
+		self.abre_porta()
+		surface.blit(self.surface, (self.x, self.y))
+
+class Porta_Direita(object):
+	def __init__(self):
+		self.surface = pygame.image.load("Imagens/portaAutomatica/porta.png").convert()
+		self.surface.set_colorkey((0,0,0))
+		
+		#posicao do puto
+		self.x = 623
+		self.y = 204
+
+	
+	def abre_porta(self):
+		self.x += 1
+		if self.x == 730:
+			self.x = 623
+
+	def set_surface(self, imagem):
+		self.surface = pygame.image.load(imagem).convert()
+		self.surface.set_colorkey((0,0,0))
+		
+	def draw(self, surface):
+		self.abre_porta()
+		surface.blit(self.surface, (self.x, self.y))
+
+
 def atualiza_tela():
 		
 	ceu.draw(screen)
 	solo.draw(screen)
 	parede.draw(screen)
 	sensor.draw(screen)
+	
+	portaEsquerda.draw(screen)
+	portaDireita.draw(screen)
+	
 	personagem.draw(screen)
+	
+	raio.draw(screen)
 	pygame.display.flip()
 	
 
@@ -131,7 +204,9 @@ solo = Solo()
 parede = Parede()
 sensor = Sensor()
 personagem = Personagem()
-
+raio = Raio()
+portaEsquerda = Porta_Esquerda()
+portaDireita = Porta_Direita()
 clock = pygame.time.Clock()
 
 running = True
