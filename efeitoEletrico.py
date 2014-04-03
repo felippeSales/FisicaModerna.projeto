5fb869aff7d90cc2ec37512aca890ca95fb94316
rodrigofariasm <rodrigohfarias@gmail.com>
2014-04-03 11:52:20 -0300

    TRILHA DO ELETRON FEITA HAHAHAHAHHA

--- efeitoEletrico.py
+++ efeitoEletrico.py
@@ -11,6 +11,38 @@ pygame.init()
 screen = pygame.display.set_mode((800,600),0,32)
 pygame.display.set_caption("Efeito Foto Eletrico")
 
+
+
+class Eletron():
+	movespeed = 1
+	def __init__(self):
+		self.surface = pygame.image.load("Imagens/eletron.png")
+		self.surface.set_colorkey((0,0,0))
+		#posicao do puto
+		self.x = 227
+		self.y = 470
+	
+	def set_surface(self, imagem):
+		self.surface = pygame.image.load(imagem).convert()
+		self.surface.set_colorkey((0,255,0))
+	def move(self):
+		if(self.x == 227 and self.y >= 470 and self.y < 540):
+			self.y += self.movespeed
+		elif(self.y ==540 and self.x >= 227 and self.x < 532):
+			self.x += self.movespeed
+		elif(self.x ==532 and self.y <= 540 and self.y > 100):
+			self.y -= self.movespeed
+		elif(self.y == 100 and self.x <= 532 and self.x > 227):
+			self.x -= self.movespeed
+		elif(self.x == 227 and self.y >= 100 and self.y < 222):
+			self.y += self.movespeed
+		else:
+			self.x = 227
+			self.y = 470
+	def draw(self, surface):
+		surface.blit(self.surface, (self.x, self.y))
+		self.move()
+
 class Raio():
 	def __init__(self):
 		self.surface = pygame.image.load("Imagens/radiacao.png").convert()
@@ -92,7 +124,8 @@ class Indicador():
 				self.sol.atualizaSol(self.pos)
 				
 				
-		
+	def getPos(self):
+		return self.pos	
 			
 	def set_surface(self, imagem):
 		self.surface = pygame.image.load(imagem).convert()
@@ -102,8 +135,13 @@ class Indicador():
 		self.sol.draw(screen, self.pos)
 		if self.pos > 0:
 			screen.blit(self.lampada_on[0],self.lampada_on[1])
+			for i in range(0, self.pos*3):
+				eletrons[i].draw(screen)
+
 		else:
 			screen.blit(self.lampada[0],self.lampada[1])
+			
+			
 
 class IndicadorH():
 	move_speed = 25
@@ -146,9 +184,7 @@ class IndicadorH():
 
 class EfeitoVoltaico():
 	circuito = (pygame.image.load("Imagens/PlacasDeNP.png"),(100,100))
-	eletron  = (pygame.image.load("Imagens/eletron.png"),(600,400))
 	back  = (pygame.image.load("Imagens/background.png"),(0,0))
-	
 	surface = pygame.image.load("Imagens/radiacao.png").convert()
 	surface.set_colorkey((0,0,0))
 	raio = (surface,(10,10))
@@ -156,11 +192,10 @@ class EfeitoVoltaico():
 	frequencia_bar = (pygame.image.load("Imagens/frequencia.png"),(550,80))
 	raio_pos = (50,50)
 	cursor_pos =(527, 37)
-	
 	intensidade_bar[0].set_colorkey((255,255,255))
 	frequencia_bar[0].set_colorkey((255,255,255))
 	circuito[0].set_colorkey((255,255,255))
-	eletron[0].set_colorkey((255,255,255))
+	
 	
 	
 	def set_background(self, imagem):
@@ -173,11 +208,9 @@ class EfeitoVoltaico():
 		screen.blit(self.intensidade_bar[0], self.intensidade_bar[1])
 		screen.blit(self.frequencia_bar[0], self.frequencia_bar[1])
 		screen.blit(self.circuito[0],self.circuito[1])
-		screen.blit(self.eletron[0],self.eletron[1])
 		
 		indicador.draw(screen)
 		indicadorh.draw(screen)
-		
 		pygame.display.flip()
 		
 	def run(self):     
@@ -195,6 +228,9 @@ class EfeitoVoltaico():
 
 
 raios = []
+eletrons = []
+for i in range(0, 100):
+	eletrons.append(Eletron())
 for i in range(0,8):
 	raios.append(Raio())
 indicador = Indicador()			
